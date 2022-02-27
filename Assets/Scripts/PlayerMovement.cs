3df45ba61983e524c53991c48fb94884ed6f747c
Sintonix <kelvinbly01@gmail.com>
2022-02-27 06:01:52 -0800

    Unit2PartB

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,6 +9,7 @@ public class PlayerMovement : MonoBehaviour
     public float speed = 10.0f;
     private float xRange = 20.0f;
 
+    public GameObject projectilePrefab;
     // Start is called before the first frame update
     void Start()
     {
@@ -29,6 +30,11 @@ public class PlayerMovement : MonoBehaviour
         if (transform.position.x < -xRange)
         {
             transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
         }
     }
 }
